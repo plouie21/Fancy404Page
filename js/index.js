334688778d1f6b7579cfa04cf078258efa45281c
Paul Cielo <plouie21@gmail.com>
2018-09-26 13:40:35 -0700

    Change input val

--- js/index.js
+++ js/index.js
@@ -161,7 +161,7 @@ function initCanvas() {
 function initInput() {
   input = document.getElementById('input');
   input.addEventListener('keyup', updateText);
-  input.value = '404 Oh No';
+  input.value = '404';
    }
 
 function updateText() {
